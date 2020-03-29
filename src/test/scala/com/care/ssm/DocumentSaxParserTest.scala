4fa6a1bdb7a096d9423ed99de69552817db03362
Massimo Caresana <ilcaresana@hotmail.it>
2020-03-29 17:27:07 +0200

    adding tests matchers

--- src/test/scala/com/care/ssm/DocumentSaxParserTest.scala
+++ src/test/scala/com/care/ssm/DocumentSaxParserTest.scala
@@ -2,20 +2,16 @@ package com.care.ssm
 
 import org.scalatest.PrivateMethodTester
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class DocumentSaxParserTest extends AnyFlatSpec with PrivateMethodTester{
+class DocumentSaxParserTest extends AnyFlatSpec with PrivateMethodTester with Matchers {
 
   "inner zip sheet file path" should " be correct " in {
 
     val parser = new DocumentSaxParser
     val buildInnerZipSheetFilePath: PrivateMethod[String] = PrivateMethod[String](Symbol("buildInnerZipSheetFilePath"))
 
-    assertResult("xl/worksheets/sheet1.xml") {
-      parser invokePrivate buildInnerZipSheetFilePath("xl/worksheets", "1")
-    }
-
-    assertResult("xl/worksheets/sheet123.xml") {
-      parser invokePrivate buildInnerZipSheetFilePath("xl/worksheets", "123")
-    }
+    "xl/worksheets/sheet1.xml" should equal (parser invokePrivate buildInnerZipSheetFilePath("xl/worksheets", "1"))
+    "xl/worksheets/sheet123.xml" should equal (parser invokePrivate buildInnerZipSheetFilePath("xl/worksheets", "123"))
   }
 }
